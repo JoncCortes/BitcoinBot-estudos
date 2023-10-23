2b9b0e8ff995f4525e6d78cd9e4d17561ca55832
Jon Cortes <joncortes66@gmail.com>
2023-10-23 10:13:21 -0300

    adicionado README e requirements

---
+++ README.md
@@ -0,0 +1,5 @@
+<h1>Bitcoin Bot para Bitstamp.net</h1>
+
+<p>https://www.bitstamp.net/</p>
+
+<p>Criado para estudo de Bot e Websocket em python.</p>
---
+++ credentials_exemple.py
@@ -0,0 +1,5 @@
+#Renomeie esse arquivo para credintials.py e utilize as credenciais de sua conta no site bitstamp.net.
+
+USERNAME = 'username'
+KEY = 'key'
+SECRET = 'secret'
